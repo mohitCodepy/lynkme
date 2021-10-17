04c825ecfcb7715ad63ac03e3aa7df84193fd2d4
mohitCodepy <mohit1511199@gmail.com>
2021-10-18 01:27:39 +0530

    created animated button and colors

--- frontend_lynkme/src/App.js
+++ frontend_lynkme/src/App.js
@@ -1,12 +1,20 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <>
-    <div className="main">
-
-    </div>
-
+      <div className="main">
+        <div className="container inner-div">
+          <div className="row">
+            <div className="col-md-6 ">
+              <button className="btn btn-warning create-btn rounded-pill">Create Zone</button>
+            </div>
+            <div className="col-md-6">
+              <button className="btn btn-danger join-btn rounded-pill">Join Zone</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }

b5bb3fa487d6330d7c348cd616d740b233ffb8e0
mohitCodepy <mohit1511199@gmail.com>
2021-12-05 23:48:18 +0530

    zone model created

--- lynkme/api/admin.py
+++ lynkme/api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CustomUser
+from .models import CustomUser, Zone
 from django.contrib import admin
 # Register your models here.
 
@@ -7,4 +7,10 @@ from django.contrib import admin
 class CustomUserAdmin(admin.ModelAdmin):
     class Meta:
         model = CustomUser
+        list_display = ('__all__')
+
+@admin.register(Zone)
+class ZoneAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Zone
         list_display = ('__all__')
--- lynkme/api/models.py
+++ lynkme/api/models.py
@@ -1,9 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+import random
 class BaseClass(models.Model):
     created_at = models.DateTimeField(auto_now = True)
 
 class CustomUser(BaseClass):
     user = models.ForeignKey(User, on_delete = models.SET_NULL, null=True)
     
+
+def get_random_code():
+    code = random.randint(000000,999999)
+    return code
+
+class Zone(models.Model):
+    zone_num = models.CharField(max_length = 6, default = " ", unique = True, blank=True)
+    def save(self, *args, **kwargs):
+        code = get_random_code()
+        if Zone.objects.filter(zone_num = code).exists():
+            get_random_code()
+        self.zone_num = code
+        return super().save(*args, **kwargs)
+    def __str__(self) -> str:
+
+        return f'{self.zone_num}'

63303a35174d93b3bb735be4b825dc9e70a8867b
mohitCodepy <mohit1511199@gmail.com>
2021-12-31 22:54:59 +0530

    num of votes

--- frontend_lynkme/src/components/CreateZone.js
+++ frontend_lynkme/src/components/CreateZone.js
@@ -1,33 +1,45 @@
 import React, { Component } from "react";
-import { Form, Row, Col, Button, Container } from "react-bootstrap";
+import { Form, Row, Col, Button, Container, FloatingLabel } from "react-bootstrap";
 import axios from "axios";
 
 
 export default class CreateZone extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
-            guestCanPause : false
+            guestCanPause: false
         }
     }
 
     createZone = async () => {
         console.log('creating the zone')
-        await axios.get(`${window.BACKEND_URL}/zone`, {headers : { 'Content-Type' : 'application/json'}}).then(console.log)
+        await axios.get(`${window.BACKEND_URL}/zone`, { headers: { 'Content-Type': 'application/json' } }).then(console.log)
     }
 
     playOption = (e) => {
-        this.setState({guestCanPause: true})
+        this.setState({ guestCanPause: true })
         console.log('this is the event on chaning the rules', e.target, this.state.guestCanPause)
-    }
-    
-    Disableplay = (e) => {
-        this.setState({guestCanPause: false})
-        console.log('this is the event on chaning the rules', e.target, this.state.guestCanPause)
-        
     }
 
+    Disableplay = (e) => {
+        this.setState({ guestCanPause: false })
+        console.log('this is the event on chaning the rules', e.target, this.state.guestCanPause)
+
+    }
+    minVotes = (e) => {
+        console.log(e.target.value, 'min value')
+        if(e.target.value < 1) {
+            e.target.value = 1;
+        }
+        this.setState({ num_of_votes : e.target.value })
+    }
+    removeBorder(e){
+        // console.log(e.target, 'this is the data after click');
+        e.target.style.boxShadow = 'none';
+        e.target.style.autoComplete = 'nope'
+        e.target.style.borderColor = 'white';
+      }
     render() {
         return (
             <div className="create-zone-card">
@@ -55,6 +67,22 @@ export default class CreateZone extends Component {
                                 />
                             </Col>
                         </Row>
+                        <Row className="my-3">
+                            <Col sm="12">
+                                <Container>
+                                    <Row>
+                                      <Col sm="12">  
+                                      {this.state.guestCanPause ?      <FloatingLabel controlId="join-zone-label-id" label="No. of votes to skip" style={{ 'color': 'white', 'fontSize': 'normal' }}>
+
+                                                <Form.Control type="number" placeholder="T1E2S3T4"
+                                                    className="join-class-input"
+                                                    style={{ 'color': 'white', 'fontWeight': 'bolder', 'backgroundColor': 'transparent', 'textAlign': 'center', 'fontSize': 'large', 'borderLeft': '0', 'borderRight': '0', 'borderTop': '0', 'borderBottomWidth': '3px', 'paddingTop': '35px', 'borderRadius': '7px' }} onFocus={this.removeBorder} autoComplete="off" onChange={this.minVotes} />
+                                            </FloatingLabel> : '' }
+                                        </Col> 
+                                    </Row>
+                                </Container>
+                            </Col>
+                        </Row> 
                     </Container>
                     <Container className="create-form-button">
                         <Row>
